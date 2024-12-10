7576dabc4703f5fc88f0b73328e9f417dff16fd9
kimchungdo <dorian21@naver.com>
2024-12-10 22:10:06 +0900

    initial commit

--- MyWeather/app/src/main/java/com/example/myweather/CityData.kt
+++ MyWeather/app/src/main/java/com/example/myweather/CityData.kt
@@ -3,5 +3,5 @@ package com.example.myweather
 import androidx.room.Entity
 
 data class CityList(var city:ArrayList<CityData>)
-@Entity(tableName = "city_list")
+@Entity(tableName = "city_list") //add DB
 data class CityData(var id: String, var name: String)
--- MyWeather/app/src/main/java/com/example/myweather/db/AppDatabase.kt
+++ MyWeather/app/src/main/java/com/example/myweather/db/AppDatabase.kt
@@ -2,7 +2,7 @@ package com.example.myweather.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-
+//add DB
 @Database(entities = [], version = 1)
 abstract AppDatabase:RoomDatabase {
 }
